fa597f4954fb042db2e658aa4a2fc23b9bada17d
Nyxthenightmare <sunmichael69@gmail.com>
2025-08-20 21:03:00 +0700

    Delete Auto Blog directory

--- Auto Blog/readability/autoload-psrlog.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/Psr/Log/LoggerInterface.php';
-require_once __DIR__ . '/Psr/Log/LoggerTrait.php';
-require_once __DIR__ . '/Psr/Log/LoggerAwareInterface.php';
-require_once __DIR__ . '/Psr/Log/LoggerAwareTrait.php';
-require_once __DIR__ . '/Psr/Log/AbstractLogger.php';
-require_once __DIR__ . '/Psr/Log/NullLogger.php';
-require_once __DIR__ . '/Psr/Log/LogLevel.php';
-require_once __DIR__ . '/Psr/Log/InvalidArgumentException.php';
